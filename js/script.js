19b044dbc5671d8a9e0ca873718b7e4292da81d5
shb627 <shb0627@naver.com>
2026-04-21 13:01:32 +0900

    회원가입 페이지

--- js/script.js
+++ js/script.js
@@ -52,3 +52,32 @@ const bannerSwiper = new Swiper(".banner-swiper", {
     disableOnInteraction: false, // 사용자가 눌러도 자동재생 계속
   },
 });
+
+// 실제 반응형 적용 (ResizeObserver 사용)
+const observer = new ResizeObserver((entries) => {
+  const width = entries[0].contentRect.width;
+  shell.classList.toggle("is-desktop", width >= 768);
+  shell.classList.toggle("is-mobile", width < 768);
+});
+observer.observe(document.body);
+
+// 비밀번호 에러
+const pw = document.getElementById("pw");
+const pwConfirm = document.getElementById("pw-confirm");
+const pwError = document.getElementById("pw-error");
+
+pwConfirm.addEventListener("input", () => {
+  if (pwConfirm.value === "") {
+    pwConfirm.classList.remove("error");
+    pwError.classList.remove("show");
+    return;
+  }
+
+  if (pw.value !== pwConfirm.value) {
+    pwConfirm.classList.add("error");
+    pwError.classList.add("show");
+  } else {
+    pwConfirm.classList.remove("error");
+    pwError.classList.remove("show");
+  }
+});
